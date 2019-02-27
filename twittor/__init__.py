220d750a3e3aa58830f16f1893bc654c4864014c
tkpromise <tkpromise@foxmail.com>
2019-02-27 14:53:15 +0800

    0.2
    Signed-off-by: tkpromise <tkpromise@foxmail.com>

--- twittor/__init__.py
+++ twittor/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
-from twittor.route import index
+from twittor.route import index, login
 
 
 def create_app():
     app = Flask(__name__)
     app.add_url_rule('/', 'index', index)
+    app.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
     return app
 
 
--- twittor/route.py
+++ twittor/route.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
+from twittor.forms import LoginForm
 
 def index():
     name = {'username':'root'}
@@ -13,4 +14,12 @@ def index():
         }
     ]
     return render_template('index.html', name=name, posts=posts)
+
+def login():
+    form = LoginForm(csrf_enabled=False)
+    if form.validate_on_submit():
+        msg = f'username={form.username.data}, password={form.password.data}, remember_me={form.remember_me.data}'
+        print(msg)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
